03c500aa1fe47be169d281491d5106d3723d5377
Clement97712 <mj.capou@gmail.com>
2018-02-08 01:11:49 -0400

    Test Version1.0 beta

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import './App.css';
+
+let marked =require("marked");
+
+class App extends Component {
+
+      state =({
+        markdown:"" 
+      });
+
+ 
+
+  update=(value)=>{
+
+    this.setState({
+      markdown:value
+    })
+
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <textarea rows="20" cols="50" className="texte" value={this.state.markdown} onChange={(event)=>{this.update(event.target.value)}}></textarea>
+        <div dangerouslySetInnerHTML={{__html:marked(this.state.markdown)}} className="box"></div>
+      </div>
+    );
+  }
+}
+
+export default App;
